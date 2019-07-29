3bd3279fe875d73c0e0165934063ad761a9bfdbe
Matias Peñas <mattff3@gmail.com>
2019-07-29 03:23:22 -0300

    Adding dueTime, dueDate and styling

--- src/pages/PageToDo.vue
+++ src/pages/PageToDo.vue
@@ -14,6 +14,30 @@
             <q-item-section>
                 <q-item-label>{{task.name}}</q-item-label>
             </q-item-section>
+            <q-item-section side>
+                <div class="row">
+                    <div class="column justify-center">
+                        <q-icon 
+                            name="event" 
+                            size="18px"
+                            class="q-mr-xs"
+                             />
+                    </div>
+
+                    <div class="column">
+                        <q-item-label 
+                            class="row justify-end"
+                            caption>
+                            {{task.dueDate}}
+                        </q-item-label>
+                        <q-item-label 
+                            class="row justify-end"
+                        caption>
+                            <small>{{task.dueTime}}</small>
+                        </q-item-label>
+                    </div>
+               </div>
+            </q-item-section>
         </q-item>
 
     </q-list>
@@ -31,17 +55,23 @@
                     {
                         id: 1,
                         name: 'Go to Shop',
-                        completed: false
+                        completed: false,
+                        dueDate: '2019/05/12',
+                        dueTime: '18:30'
                     },
                     {
                         id: 2,
                         name: 'Get appels',
-                        completed: true
+                        completed: true,
+                        dueDate: '2019/05/13',
+                        dueTime: '14:00'
                     },
                     {
                         id: 3,
                         name: 'Go to the cinema',
-                        completed: false
+                        completed: false,
+                        dueDate: '2019/05/14',
+                        dueTime: '19:30'
                     }
                 ]
             }
